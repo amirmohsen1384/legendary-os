32b5ac6b598ba1922e2540f9d26c1c82456fcc5f
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-07 20:32:26 +0330

    Implemented the task queue

---
+++ src/core/taskqueue.h
@@ -0,0 +1,139 @@
+#ifndef TASKQUEUE_H
+#define TASKQUEUE_H
+
+#include <vector>
+#include <cstdint>
+#include <stdexcept>
+#include <algorithm>
+#include "models/processinfo.h"
+
+class TaskQueue
+{
+protected:
+    int64_t parent(int64_t nodeIndex)
+    {
+        return (nodeIndex - 1) / 2;
+    }
+
+    int64_t left(int64_t nodeIndex)
+    {
+        return nodeIndex * 2 + 1;
+    }
+
+    int64_t right(int64_t nodeIndex)
+    {
+        return nodeIndex * 2 + 2;
+    }
+
+    bool hasLeft(int64_t nodeIndex)
+    {
+        return left(nodeIndex) < container.size();
+    }
+
+    bool hasRight(int64_t nodeIndex)
+    {
+        return right(nodeIndex) < container.size();
+    }
+
+    void swap(int64_t firstNode, int64_t secondNode)
+    {
+        if (firstNode < 0 || firstNode >= container.size())
+        {
+            return;
+        }
+        else if (secondNode < 0 || secondNode >= container.size())
+        {
+            return;
+        }
+        std::swap(container[firstNode], container[secondNode]);
+    }
+
+    void upheap(int64_t nodeIndex)
+    {
+        int64_t currentIndex = nodeIndex;
+        while (currentIndex > 0)
+        {
+            int64_t parentIndex = parent(currentIndex);
+            if (container[currentIndex]->getPriority() < container[parentIndex]->getPriority())
+            {
+                break;
+            }
+            swap(currentIndex, parent(currentIndex));
+            currentIndex = parentIndex;
+        }
+    }
+
+    void downheap(int64_t nodeIndex)
+    {
+        while (hasLeft(nodeIndex))
+        {
+            int64_t leftIndex = left(nodeIndex);
+            int64_t smallChildIndex = leftIndex;
+            if (hasRight(nodeIndex))
+            {
+                int64_t rightIndex = right(nodeIndex);
+                if (container[leftIndex]->getPriority() <= container[rightIndex]->getPriority())
+                {
+                    smallChildIndex = rightIndex;
+                }
+            }
+            if (container[smallChildIndex]->getPriority() <= container[nodeIndex]->getPriority())
+            {
+                break;
+            }
+            swap(nodeIndex, smallChildIndex);
+            nodeIndex = smallChildIndex;
+        }
+    }
+
+public:
+    bool isEmpty() const
+    {
+        return size() == 0;
+    }
+
+    size_t size() const
+    {
+        return container.size();
+    }
+
+    void insert(ProcessInfo *info)
+    {
+        if (info == nullptr)
+        {
+            throw std::invalid_argument("Cannot insert null process");
+        }
+        container.push_back(info);
+        upheap(size() - 1);
+    }
+    ProcessInfo* min()
+    {
+        if (isEmpty())
+        {
+            throw std::out_of_range("The heap is empty");
+        }
+        return container.front();
+    }
+
+    ProcessInfo* removeMin()
+    {
+        if (isEmpty())
+        {
+            throw std::out_of_range("The heap is empty");
+        }
+        auto result = container.front();
+        swap(0, container.size() - 1);
+        container.pop_back();
+        if (!isEmpty())
+        {
+            downheap(0);
+        }
+        return result;
+    }
+
+private:
+    std::vector<ProcessInfo*> container;
+};
+
+
+#endif // TASKQUEUE_H
