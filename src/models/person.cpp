e7e39e83268c31745c2ff736178d93c34c80f5ac
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-09 19:15:29 +0330

    Defined a person
    This reprsents the resources required by processes

---
+++ src/models/person.cpp
@@ -0,0 +1,114 @@
+#include "person.h"
+
+Person::Person(Person *parent)
+{
+    this->parent = parent;
+}
+
+Person::Role Person::getRole() const
+{
+    return role;
+}
+
+qint64 Person::getIdentifier() const
+{
+    return identifier;
+}
+
+QString Person::getFirstName() const
+{
+    return firstName;
+}
+
+QString Person::getLastName() const
+{
+    return lastName;
+}
+
+QString Person::getFullName() const
+{
+    return QString("%1 %2").arg(firstName, lastName);
+}
+
+Person *Person::getChild(int row)
+{
+    if (row < 0 || row >= children.size()) {
+        return nullptr;
+    }
+    return children.at(row).get();
+}
+
+QPixmap Person::getPhoto() const
+{
+    return photo;
+}
+
+qint64 Person::getSalary() const
+{
+    return salary;
+}
+
+QDate Person::getBirthday() const
+{
+    return birthday;
+}
+
+Person *Person::getParent()
+{
+    return parent;
+}
+
+void Person::setRole(Role value)
+{
+    role = value;
+}
+
+void Person::setIdentifier(qint64 value)
+{
+    identifier = value;
+}
+
+void Person::setFirstName(const QString &value)
+{
+    firstName = value;
+}
+
+void Person::setLastName(const QString &value)
+{
+    lastName = value;
+}
+
+void Person::setPhoto(const QPixmap &value)
+{
+    photo = value;
+}
+
+void Person::setSalary(qint64 value)
+{
+    salary = value;
+}
+
+void Person::setBirthday(const QDate &value)
+{
+    birthday = value;
+}
+
+void Person::setParent(Person *value)
+{
+    parent = value;
+}
+
+void Person::addChild(std::unique_ptr<Person> item)
+{
+    if (item) {
+        children.push_back(std::move(item));
+    }
+}
+
+void Person::removeChild(int row)
+{
+    if (row < 0 || row >= children.size()) {
+        return;
+    }
+    return children.erase(children.begin() + row);
+}
---
+++ src/models/person.h
@@ -0,0 +1,80 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <QString>
+#include <QPixmap>
+#include <QDate>
+
+class Person
+{
+public:
+    enum class Role {
+        Unknown = 0,
+        CEO,
+        Manager,
+        Supervisor,
+        Accountant,
+        ProductOwner,
+        ProductManager,
+        ContentCreator,
+        PromptEngineer,
+        SoftwareDeveloper
+    };
+
+public:
+    Person(Person *parent = nullptr);
+
+    Person::Role getRole() const;
+
+    qint64 getIdentifier() const;
+
+    QString getFirstName() const;
+
+    QString getLastName() const;
+
+    QString getFullName() const;
+
+    Person* getChild(int row);
+
+    QPixmap getPhoto() const;
+
+    qint64 getSalary() const;
+
+    QDate getBirthday() const;
+
+    Person* getParent();
+
+public:
+    void setRole(Person::Role value);
+
+    void setIdentifier(qint64 value);
+
+    void setFirstName(const QString &value);
+
+    void setLastName(const QString &value);
+
+    void setPhoto(const QPixmap &value);
+
+    void setSalary(qint64 value);
+
+    void setBirthday(const QDate &value);
+
+    void setParent(Person* value);
+
+    void addChild(std::unique_ptr<Person> item);
+
+    void removeChild(int row);
+
+private:
+    std::vector<std::unique_ptr<Person>> children;
+    Person::Role role = Person::Role::Unknown;
+    Person* parent = nullptr;
+    qint64 identifier;
+    QString firstName;
+    QString lastName;
+    QDate birthday;
+    qint64 salary;
+    QPixmap photo;
+};
+
+#endif // PERSON_H
