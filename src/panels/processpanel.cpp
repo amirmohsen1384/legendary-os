c3da1f33e92043569cf1bd8d30f5b51817e06637
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-09 16:29:22 +0330

    Added the process edit panel

---
+++ src/panels/processpanel.cpp
@@ -0,0 +1,99 @@
+#include "processpanel.h"
+#include "ui_processpanel.h"
+
+ProcessPanel::ProcessPanel(ProcessModel *model, QWidget *parent) : QDialog(parent)
+{
+    ui = std::make_unique<Ui::ProcessPanel>();
+    ui->setupUi(this);
+    setModel(model);
+}
+
+ProcessPanel::~ProcessPanel() {}
+
+QString ProcessPanel::getName() const
+{
+    return ui->nameEdit->text();
+}
+
+void ProcessPanel::setName(const QString &value)
+{
+    ui->nameEdit->setText(value);
+}
+
+qint64 ProcessPanel::getPriority() const
+{
+    return ui->priorityEdit->value();
+}
+
+void ProcessPanel::setPriority(qint64 value)
+{
+    ui->priorityEdit->setValue(value);
+}
+
+qint64 ProcessPanel::getBurstTime() const
+{
+    return ui->burstEdit->value();
+}
+
+void ProcessPanel::setBurstTime(qint64 value)
+{
+    ui->burstEdit->setValue(value);
+}
+
+QString ProcessPanel::getFileName() const
+{
+    return ui->fileEdit->text();
+}
+
+void ProcessPanel::setFileName(const QString &value)
+{
+    ui->fileEdit->setText(value);
+}
+
+bool ProcessPanel::dependsOnFile() const
+{
+    return ui->fileGroup->isChecked();
+}
+
+void ProcessPanel::setDependsOnFile(bool value)
+{
+    ui->fileGroup->setChecked(value);
+}
+
+ProcessInfo ProcessPanel::getProcessInfo() const
+{
+    ProcessInfo info;
+    info.setName(getName());
+    info.setFileName(getFileName());
+    info.setPriority(getPriority());
+    info.setBurstTime(getBurstTime());
+    return info;
+}
+
+void ProcessPanel::setProcessInfo(const ProcessInfo &info)
+{
+    setName(info.getName());
+    setPriority(info.getPriority());
+    setFileName(info.getFileName());
+    setBurstTime(info.getBurstTime());
+}
+
+QModelIndex ProcessPanel::getParent() const
+{
+    return ui->locationView->currentIndex();
+}
+
+void ProcessPanel::setParent(const QModelIndex &parent)
+{
+    ui->locationView->setCurrentIndex(parent);
+}
+
+ProcessModel *ProcessPanel::getModel()
+{
+    return ui->locationView->model();
+}
+
+void ProcessPanel::setModel(ProcessModel *model)
+{
+    ui->locationView->setModel(model);
+}
---
+++ src/panels/processpanel.h
@@ -0,0 +1,47 @@
+#ifndef PROCESSPANEL_H
+#define PROCESSPANEL_H
+
+#include <QDialog>
+#include "models/processmodel.h"
+
+namespace Ui
+{
+    class ProcessPanel;
+}
+
+class ProcessPanel : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit ProcessPanel(ProcessModel* model, QWidget *parent = nullptr);
+    ~ProcessPanel();
+
+    QString getName() const;
+    void setName(const QString &value);
+
+    qint64 getPriority() const;
+    void setPriority(qint64 value);
+
+    qint64 getBurstTime() const;
+    void setBurstTime(qint64 value);
+
+    QString getFileName() const;
+    void setFileName(const QString &value);
+
+    bool dependsOnFile() const;
+    void setDependsOnFile(bool value);
+
+    ProcessInfo getProcessInfo() const;
+    void setProcessInfo(const ProcessInfo &info);
+
+    QModelIndex getParent() const;
+    void setParent(const QModelIndex &parent);
+
+    ProcessModel* getModel();
+    void setModel(ProcessModel *model);
+
+private:
+    std::unique_ptr<Ui::ProcessPanel> ui;
+};
+
+#endif // PROCESSPANEL_H
