7a9f55d803c73fef7d5cc529a2787d09349fdcca
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-10 18:37:17 +0330

    Implemented the person model

--- src/models/personmodel.cpp
+++ src/models/personmodel.cpp
@@ -1,8 +1,26 @@
 #include "personmodel.h"
 
+std::unique_ptr<Person> PersonModel::createPerson(const PersonInfo &info, Person *parent)
+{
+    if (!parent)
+    {
+        return {};
+    }
+    auto process = std::make_unique<Person>(parent);
+    process->setFirstName(info.getFirstName());
+    process->setBiography(info.getBiography());
+    process->setLastName(info.getLastName());
+    process->setBirthday(info.getBirthday());
+
+    auto result = process.get();
+    parent->addChild(std::move(process));
+
+    return result;
+}
+
 PersonModel::PersonModel(QObject *parent) : QAbstractItemModel(parent)
 {
-    root = std::make_unique<Person>(nullptr);
+    root = std::make_unique<Person>();
 }
 
 QModelIndex PersonModel::index(int row, int column, const QModelIndex &parent) const
@@ -14,7 +32,7 @@ QModelIndex PersonModel::index(int row, int column, const QModelIndex &parent) c
     auto ancestor = parent.isValid() ? static_cast<Person*>(parent.internalPointer()) : root.get();
     if (!ancestor)
     {
-        ancestor = root;
+        ancestor = root.get();
     }
     auto item = ancestor->getChild(row);
     return !item ? QModelIndex() : createIndex(row, 0, item);
@@ -27,6 +45,10 @@ QModelIndex PersonModel::parent(const QModelIndex &index) const
         return {};
     }
     auto item = static_cast<Person*>(index.internalPointer());
+    if (!item)
+    {
+        return {};
+    }
     auto parent = item->getParent();
     if (parent == nullptr || parent == root.get())
     {
@@ -41,7 +63,7 @@ QModelIndex PersonModel::parent(const QModelIndex &index) const
 int PersonModel::rowCount(const QModelIndex &parent) const
 {
     auto ancestor = !parent.isValid() ? root.get() : static_cast<Person*>(parent.internalPointer());
-    return !ancestor ? 0 : ancestor->childCount();
+    return ancestor ? ancestor->childCount() : 0;
 }
 
 QVariant PersonModel::headerData(int section, Qt::Orientation orientation, int role) const
@@ -61,14 +83,6 @@ QVariant PersonModel::headerData(int section, Qt::Orientation orientation, int r
         {
             return "Name";
         }
-        case PersonModel::Header::Job:
-        {
-            return "Job";
-        }
-        case PersonModel::Header::ID:
-        {
-            return "ID";
-        }
         case PersonModel::Header::Birthday:
         {
             return "Birthday";
@@ -78,6 +92,10 @@ QVariant PersonModel::headerData(int section, Qt::Orientation orientation, int r
             return {};
         }
         }
+    }
+    case Qt::BackgroundRole:
+    {
+        return QColor(220, 220, 220);
     }
     case Qt::TextAlignmentRole:
     {
@@ -92,97 +110,31 @@ QVariant PersonModel::headerData(int section, Qt::Orientation orientation, int r
 
 int PersonModel::columnCount(const QModelIndex &parent) const
 {
-    if (!parent.isValid())
-    {
-        return 0;
-    }
-
-    int count = 0;
-
-    // Name
-    count++;
-
-    // Birthday
-    count++;
-
-    // Job
-    count++;
-
-    // Identifier
-    count++;
-
-    return count;
+    auto ancestor = !parent.isValid() ? root.get() : static_cast<Person*>(parent.internalPointer());
+    return ancestor ? ancestor->columnCount() : 0;
 }
 
 QVariant PersonModel::data(const QModelIndex &index, int role) const
 {
-    if (!index.isValid()) {
+    if (!index.isValid())
+    {
         return {};
     }
     auto item = static_cast<Person*>(index.internalPointer());
+    if(!item)
+    {
+        return {};
+    }
     switch (role)
     {
     case Qt::DisplayRole:
     {
         auto group = static_cast<PersonModel::Header>(index.column());
-        switch(group) {
+        switch(group)
+        {
         case PersonModel::Header::Name:
         {
             return item->getFullName();
-        }
-        case PersonModel::Header::Job:
-        {
-            switch(item->getJob())
-            {
-            case Person::Job::CEO:
-            {
-                return "Chief Executive Officer";
-            }
-            case Person::Job::Accountant:
-            {
-                return "Accountant";
-            }
-            case Person::Job::ContentCreator:
-            {
-                return "Content Creator";
-            }
-            case Person::Job::Manager:
-            {
-                return "Manager";
-            }
-            case Person::Job::ProductManager:
-            {
-                return "Product Manager";
-            }
-            case Person::Job::ProductOwner:
-            {
-                return "Product Owner";
-            }
-            case Person::Job::PromptEngineer:
-            {
-                return "Prompt Engineer";
-            }
-            case Person::Job::SoftwareDeveloper:
-            {
-                return "Software Developer";
-            }
-            case Person::Job::Supervisor:
-            {
-                return "Supervisor";
-            }
-            case Person::Job::Unknown:
-            {
-                return "Not Known";
-            }
-            default:
-            {
-                return {};
-            }
-            }
-        }
-        case PersonModel::Header::ID:
-        {
-            return item->getIdentifier();
         }
         case PersonModel::Header::Birthday:
         {
@@ -192,11 +144,117 @@ QVariant PersonModel::data(const QModelIndex &index, int role) const
         {
             return {};
         }
+        }
     }
-    case Qt::DecorationRole:
+    case Qt::ToolTipRole:
     {
-        const auto &photo = item->getPhoto();
-        return photo.isNull() ? QPixmap() : photo.scaled(64, 64);
+        const auto biography = item->getBiography();
+        return biography.size() <= 24 ? biography : QString("%1...").arg(biography.first(24));
+    }
+    case Qt::TextAlignmentRole:
+    {
+        return Qt::AlignCenter;
+    }
+    case Person::FirstNameRole:
+    {
+        return item->getFirstName();
+    }
+    case Person::LastNameRole:
+    {
+        return item->getLastName();
+    }
+    case Person::BirthdayRole:
+    {
+        return item->getBirthday();
+    }
+    case Person::BiographyRole:
+    {
+        return item->getBiography();
+    }
+    default:
+    {
+        return {};
     }
     }
+}
+
+bool PersonModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    auto changed = false;
+    if (!value.isValid())
+    {
+        return changed;
+    }
+    auto item = static_cast<Person*>(index.internalPointer());
+    switch(role)
+    {
+    case Person::FirstNameRole:
+    {
+        if (value.canConvert<QString>())
+        {
+            item->setFirstName(value.toString());
+            changed = true;
+        }
+        break;
+    }
+    case Person::LastNameRole:
+    {
+        if (value.canConvert<QString>())
+        {
+            item->setLastName(value.toString());
+            changed = true;
+        }
+    }
+    case Person::BiographyRole:
+    {
+        if (value.canConvert<QString>())
+        {
+            item->setBiography(value.toString());
+            changed = true;
+        }
+    }
+    case Person::BirthdayRole:
+    {
+        if (value.canConvert<QDate>())
+        {
+            item->setBirthday(value.toDate());
+            changed = true;
+        }
+    }
+    }
+    if (changed)
+    {
+        emit dataChanged(index, index, {role, Qt::DisplayRole, Qt::ToolTipRole});
+    }
+    return changed;
+}
+
+void PersonModel::insert(const PersonInfo &data, const QModelIndex &parent)
+{
+    auto ancestor = !parent.isValid() ? root.get() : static_cast<Person*>(parent.internalPointer());
+    if(!ancestor)
+    {
+        return false;
+    }
+    beginInsertRows(parent, ancestor->childCount() + 1, ancestor->childCount() + 1);
+    auto person = createPerson(info, ancestor);
+    endInsertRows();
+    return person != nullptr;
+}
+
+void PersonModel::remove(const QModelIndex &index)
+{
+    if (!index.isValid())
+    {
+        return false;
+    }
+    auto ancestor = static_cast<Person*>(index.parent().internalPointer());
+    if(!ancestor || index.row() < 0 || index.row() >= ancestor->childCount())
+    {
+        return false;
+    }
+    beginRemoveRows(index.parent(), index.row(), index.row());
+    ancestor->removeChild(index.row());
+    endRemoveRows();
+    return true;
 }
--- src/models/personmodel.h
+++ src/models/personmodel.h
@@ -7,24 +7,27 @@
 class PersonModel : public QAbstractItemModel
 {
     Q_OBJECT
+
+    std::unique_ptr<Person> createPerson(const PersonInfo &info, Person *parent);
+
 public:
-    enum class Header
-    {
-        Name = 0,
-        ID,
-        Birthday,
-        Job
-    };
+    enum class Header {Name = 0, Birthday = 1};
+
     explicit PersonModel(QObject *parent = nullptr);
 
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &index) const override;
+    virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
+    virtual QModelIndex parent(const QModelIndex &index) const override;
 
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    virtual int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const override;
 
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    virtual bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
+    virtual void insert(const PersonInfo &data, const QModelIndex &parent);
+    virtual void remove(const QModelIndex &index);
 
 private:
     std::unique_ptr<Person> root;
