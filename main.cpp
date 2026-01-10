3656c217e36d380ae7d4d9d3bc5239c579f16984
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-10 21:24:03 +0330

    Performing Test 1

--- main.cpp
+++ main.cpp
@@ -1,46 +1,23 @@
 #include <QMessageBox>
 #include <QApplication>
 #include "core/system.h"
+#include "models/processmodel.h"
+#include "panels/processpanel.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    Config::Container settings;
-    try
-    {
-        Storage::initialize();
-        if (!Config::exists())
-        {
-            throw Exception::ConfigNotFound();
-        }
-        settings = Config::load();
-    }
-    catch (const Exception::ConfigNotFound &error)
-    {
-        QMessageBox::information(
-            nullptr,
-            "No configuration found",
-            QString("%1\n\nWe have initialized the system with default data.").arg(error.what())
-        );
-    }
-    catch (const Exception::ConfigCorruption &error)
-    {
-        QMessageBox::warning(
-            nullptr,
-            "Configuration corrupted",
-            QString("%1\n\nThe configuration file will be reset back to the default.").arg(error.what())
-        );
-    }
-    catch(...)
-    {
-        QMessageBox::critical(
-            nullptr,
-            "Unknown Error",
-            "An unknown error has happened. The application will be closed."
-        );
-        QApplication::quit();
-    }
+    ProcessModel model;
 
+    while (true)
+    {
+        ProcessPanel panel(&model);
+        if(panel.exec() == QDialog::Accepted)
+        {
+            auto info = panel.getProcessInfo();
+            model.insert(info);
+        }
+    }
     return app.exec();
 }
