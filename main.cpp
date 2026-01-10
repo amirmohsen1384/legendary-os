ca61e5dc26accac2f4ae80f2343a6b5b671bdca5
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-10 23:54:24 +0330

    Implemented Test 2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "core/system.h"
 #include "models/processmodel.h"
 #include "panels/processpanel.h"
+#include <QTreeView>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
@@ -20,8 +22,36 @@ int main(int argc, char *argv[])
         }
         else
         {
-            return 0;
+            break;
         }
     }
-    return app.exec();
+
+    while (true)
+    {
+        QEventLoop loop;
+        QTreeView view;
+        view.setModel(&model);
+        view.show();
+
+        QPushButton button("Delete");
+        button.show();
+
+        QObject::connect(&button, &QPushButton::clicked,
+        [&]()
+        {
+            model.remove(view.currentIndex());
+        }
+        );
+
+        QPushButton close("Close");
+        close.show();
+
+        QObject::connect(&close, &QPushButton::clicked, &loop, &QEventLoop::exit);
+
+        if(!loop.exec())
+        {
+            break;
+        }
+    }
+    return 0;
 }
