51f7a5f86d7526474a8e05117e119c80e9ead61d
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-11 13:56:08 +0330

    Removed the test

--- main.cpp
+++ main.cpp
@@ -9,51 +9,5 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
-    QModelIndex index;
-    ProcessModel model;
-    while (true)
-    {
-        ProcessPanel panel(&model);
-        panel.expandFrom(index);
-        if(panel.exec() == QDialog::Accepted)
-        {
-            auto info = panel.getProcessInfo();
-            model.insert(info, panel.getParent());
-            index = panel.getParent();
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    while (true)
-    {
-        QEventLoop loop;
-        QTreeView view;
-        view.setModel(&model);
-        view.show();
-
-        QPushButton button("Delete");
-        button.show();
-
-        QObject::connect(&button, &QPushButton::clicked,
-        [&]()
-        {
-            model.remove(view.currentIndex());
-        }
-        );
-
-        QPushButton close("Close");
-        close.show();
-
-        QObject::connect(&close, &QPushButton::clicked, &loop, &QEventLoop::exit);
-
-        if(!loop.exec())
-        {
-            break;
-        }
-    }
     return 0;
 }
