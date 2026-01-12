eb901b5159f59509a4a362969f3498c4a9ef2ad7
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-12 17:25:05 +0330

    Implemented the agent editor

--- src/dialogs/agentedit.cpp
+++ src/dialogs/agentedit.cpp
@@ -1,14 +1,77 @@
+#include <QMessageBox>
 #include "agentedit.h"
 #include "ui_agentedit.h"
 
-AgentEdit::AgentEdit(QWidget *parent)
-    : QDialog(parent)
-    , ui(new Ui::AgentEdit)
+AgentEdit::AgentEdit(AgentModel *model, QWidget *parent) : AgentEdit(parent)
 {
+    setModel(model);
+}
+
+AgentEdit::AgentEdit(QWidget *parent) : QDialog(parent)
+{
+    ui = std::make_unique<Ui::AgentEdit>();
     ui->setupUi(this);
 }
 
 AgentEdit::~AgentEdit()
 {
     delete ui;
+}
+
+QString AgentEdit::getName() const
+{
+    return ui->nameEdit->text();
+}
+
+void AgentEdit::setName(const QString &value)
+{
+    ui->nameEdit->setText(value);
+}
+
+QString AgentEdit::getDescription() const
+{
+    return ui->descriptionEdit->text();
+}
+
+void AgentEdit::setDescription(const QString &value)
+{
+    ui->descriptionEdit->setText(value);
+}
+
+QModelIndex AgentEdit::getParent() const
+{
+    return ui->agentView->currentIndex();
+}
+
+void AgentEdit::setParent(const QModelIndex &index)
+{
+    ui->agentView->setCurrentIndex(index);
+}
+
+AgentModel *AgentEdit::getModel() const
+{
+    return ui->agentView->model();
+}
+
+void AgentEdit::setModel(AgentModel *model)
+{
+    ui->agentView->setModel(model);
+}
+
+void AgentEdit::expandFrom(const QModelIndex &index)
+{
+    ui->agentView->expand(index);
+}
+
+void AgentEdit::accept()
+{
+    if (getName().isEmpty())
+    {
+        QMessageBox::warning(
+            this, "No name provided",
+            "You have set a name for the agent.\n\n"
+            "Agents need to be associated with a name in the agent hiearchy.");
+        return;
+    }
+    QDialog::accept();
 }
--- src/dialogs/agentedit.h
+++ src/dialogs/agentedit.h
@@ -2,6 +2,7 @@
 #define AGENTEDIT_H
 
 #include <QDialog>
+#include "models/agentmodel.h"
 
 namespace Ui
 {
@@ -13,11 +14,29 @@ class AgentEdit : public QDialog
     Q_OBJECT
 
 public:
+    explicit AgentEdit(AgentModel *model, QWidget *parent = nullptr);
     explicit AgentEdit(QWidget *parent = nullptr);
     ~AgentEdit();
 
+    QString getName() const;
+    void setName(const QString &value);
+
+    QString getDescription() const;
+    void setDescription(const QString &value);
+
+    QModelIndex getParent() const;
+    void setParent(const QModelIndex &index);
+
+    AgentModel* getModel() const;
+    void setModel(AgentModel *model);
+
+    void expandFrom(const QModelIndex &index);
+
+public:
+    virtual void accept();
+
 private:
-    Ui::AgentEdit *ui;
+    std::unique_ptr<Ui::AgentEdit> ui;
 };
 
 #endif // AGENTEDIT_H
