5187e4ee65acd4b984f58c5d2f55bf1c5f844002
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-13 20:45:13 +0330

    Completed the ready task queue model

--- src/models/readytaskmodel.cpp
+++ src/models/readytaskmodel.cpp
@@ -1,45 +1,288 @@
 #include "readytaskmodel.h"
+#include "task.h"
 
-ReadyTaskModel::ReadyTaskModel(QObject *parent)
-    : QAbstractItemModel(parent)
-{}
+qint64 ReadyTaskModel::left(qint64 node)
+{
+    return node * 2 + 1;
+}
+
+qint64 ReadyTaskModel::right(qint64 node)
+{
+    return node * 2 + 2;
+}
+
+bool ReadyTaskModel::hasLeft(qint64 node)
+{
+    return left(node) < container.size();
+}
+
+qint64 ReadyTaskModel::ancestor(qint64 node)
+{
+    return (node - 1) / 2;
+}
+
+bool ReadyTaskModel::hasRight(qint64 node)
+{
+    return right(node) < container.size();
+}
+
+void ReadyTaskModel::swap(qint64 one, qint64 two)
+{
+    if (one < 0 || one >= container.size())
+    {
+        return;
+    }
+    else if (two < 0 || two >= container.size())
+    {
+        return;
+    }
+    else
+    {
+        std::swap(container[one], container[two]);
+        emit dataChanged(index(one, 0), index(one, columnCount() - 1), {Qt::DisplayRole});
+        emit dataChanged(index(two, 0), index(two, columnCount() - 1), {Qt::DisplayRole});
+    }
+}
+
+void ReadyTaskModel::upheap(qint64 node)
+{
+    qint64 top = ancestor(node);
+    while (top >= 0)
+    {
+        const auto topPriority = container[top].data(Task::PriorityRole).toLongLong();
+        const auto nodePriority = container[node].data(Task::PriorityRole).toLongLong();
+        if (topPriority < nodePriority)
+        {
+            swap(top, node);
+            node = top;
+            top = ancestor(node);
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+void ReadyTaskModel::downheap(qint64 node)
+{
+    while (hasLeft(node))
+    {
+        qint64 larger = left(node);
+        if (hasRight(node))
+        {
+            auto rightNode = right(node);
+            const auto rightPriority = container[rightNode].data(Task::PriorityRole).toLongLong();
+            const auto largerPriority = container[larger].data(Task::PriorityRole).toLongLong();
+            if (largerPriority < rightPriority)
+            {
+                larger = rightNode;
+            }
+        }
+        qint64 parent = node;
+        qint64 child = larger;
+        const auto childPriority = container[child].data(Task::PriorityRole).toLongLong();
+        const auto parentPriority = container[parent].data(Task::PriorityRole).toLongLong();
+        if (parentPriority < childPriority)
+        {
+            swap(child, parent);
+            node = larger;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+ReadyTaskModel::ReadyTaskModel(qsizetype maximum, QObject *parent) : QAbstractListModel(parent)
+{
+}
 
 QVariant ReadyTaskModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    // FIXME: Implement me!
+    switch (orientation)
+    {
+    case Qt::Vertical:
+    {
+        switch (role)
+        {
+        case Qt::DisplayRole:
+        {
+            return section + 1;
+        }
+        case Qt::BackgroundRole:
+        {
+            return QColor(220, 220, 220);
+        }
+        case Qt::TextAlignmentRole:
+        {
+            return Qt::AlignCenter;
+        }
+        default:
+        {
+            return {};
+        }
+        }
+    }
+    case Qt::Horizontal:
+    {
+        switch(role)
+        {
+        case Qt::DisplayRole:
+        {
+            auto group = static_cast<Header>(section);
+            switch (group)
+            {
+            case Header::Name:
+            {
+                return "Name";
+            }
+            case Header::PID:
+            {
+                return "PID";
+            }
+            case Header::Priority:
+            {
+                return "Priority";
+            }
+            case Header::Progress:
+            {
+                return "Progress";
+            }
+            }
+        }
+        case Qt::BackgroundRole:
+        {
+            return QColor(220, 220, 220);
+        }
+        case Qt::TextAlignmentRole:
+        {
+            return Qt::AlignCenter;
+        }
+        }
+    }
+    }
 }
 
-QModelIndex ReadyTaskModel::index(int row, int column, const QModelIndex &parent) const
+qsizetype ReadyTaskModel::getMaximumSize() const
 {
-    // FIXME: Implement me!
+    return maximum;
 }
 
-QModelIndex ReadyTaskModel::parent(const QModelIndex &index) const
+bool ReadyTaskModel::insertTask(const QPersistentModelIndex &index)
 {
-    // FIXME: Implement me!
+    auto size = container.size();
+    if (size >= maximum)
+    {
+        return false;
+    }
+    beginInsertRows(QModelIndex(), size, size);
+    container.append(index);
+    endInsertRows();
+    upheap(size);
+    return true;
+}
+
+void ReadyTaskModel::removeMostCritical()
+{
+    if (!container.isEmpty())
+    {
+        auto size = container.size();
+        beginRemoveRows(QModelIndex(), size - 1, size - 1);
+        auto data = container.takeAt(size - 1);
+        endRemoveRows();
+        if (!container.isEmpty())
+        {
+            container[0] = data;
+            downheap(0);
+        }
+    }
+}
+
+QPersistentModelIndex ReadyTaskModel::getMostCritical() const
+{
+    return container.front();
+}
+
+
+void ReadyTaskModel::setMaximumSize(qsizetype size)
+{
+    maximum = size;
+    container.reserve(size);
 }
 
 int ReadyTaskModel::rowCount(const QModelIndex &parent) const
 {
     if (!parent.isValid())
+    {
         return 0;
-
-    // FIXME: Implement me!
+    }
+    return container.size();
 }
 
 int ReadyTaskModel::columnCount(const QModelIndex &parent) const
 {
     if (!parent.isValid())
+    {
         return 0;
+    }
 
-    // FIXME: Implement me!
+    auto count = 0;
+
+    // Name
+    count++;
+
+    // PID
+    count++;
+
+    // Progress
+    count++;
+
+    // Priority
+    count++;
+
+    return count;
 }
 
 QVariant ReadyTaskModel::data(const QModelIndex &index, int role) const
 {
     if (!index.isValid())
-        return QVariant();
-
-    // FIXME: Implement me!
-    return QVariant();
+    {
+        return {};
+    }
+    auto target = container.at(index.row());
+    switch (role)
+    {
+    case Qt::DisplayRole:
+    {
+        auto group = static_cast<Header>(index.column());
+        switch (group)
+        {
+        case Header::Name:
+        {
+            return target.data(Task::NameRole).toString();
+        }
+        case Header::PID:
+        {
+            return target.data(Task::PIDRole).toLongLong();
+        }
+        case Header::Priority:
+        {
+            auto priority = target.data(Task::PriorityRole).toLongLong();
+            return QString("%1%").arg(priority);
+        }
+        case Header::Progress:
+        {
+            auto burst = target.data(Task::BurstTimeRole).toLongLong();
+            auto remaining = target.data(Task::RemainingTimeRole).toLongLong();
+            return QString("%1%").arg((1 - remaining / burst) * 100);
+        }
+        }
+    }
+    case Qt::TextAlignmentRole:
+    {
+        return Qt::AlignCenter;
+    }
+    }
 }
--- src/models/readytaskmodel.h
+++ src/models/readytaskmodel.h
@@ -2,22 +2,50 @@
 #define READYTASKMODEL_H
 
 #include <QAbstractItemModel>
+#include <QPersistentModelIndex>
 
 class ReadyTaskModel : public QAbstractTableModel
 {
     Q_OBJECT
+    enum class Header {PID, Name, Priority, Progress};
+
+protected:
+    void upheap(qint64 node);
+
+    qint64 left(qint64 node);
+
+    qint64 right(qint64 node);
+
+    bool hasLeft(qint64 node);
+
+    void downheap(qint64 node);
+
+    bool hasRight(qint64 node);
+
+    qint64 ancestor(qint64 node);
+
+    void swap(qint64 one, qint64 two);
 
 public:
-    explicit ReadyTaskModel(QObject *parent = nullptr);
+    explicit ReadyTaskModel(qsizetype maximum = 0, QObject *parent = nullptr);
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    virtual int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    qsizetype getMaximumSize() const;
+    QPersistentModelIndex getMostCritical() const;
+
+public slots:
+    bool insertTask(const QPersistentModelIndex &index);
+    void setMaximumSize(qsizetype size);
+    void removeMostCritical();
 
 private:
-    QModelIndexList container;
+    qsizetype maximum;
+    QList<QPersistentModelIndex> container;
 };
 
 #endif // READYTASKMODEL_H
