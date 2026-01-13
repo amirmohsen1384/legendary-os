85412602a2082198d0681e9278831007ca7ba977
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-13 22:51:27 +0330

    Revised the kernel

--- src/core/kernel.cpp
+++ src/core/kernel.cpp
@@ -12,7 +12,6 @@ CentralKernel::CentralKernel(const Config::Info &info, QObject *parent) : QObjec
 
 void CentralKernel::addTask(const TaskInfo &info, const QModelIndex &parent)
 {
-
 }
 
 void CentralKernel::removeTask(const QModelIndex &index)
@@ -48,6 +47,33 @@ bool CentralKernel::isLocked() const
     return locked;
 }
 
+TaskModel *CentralKernel::taskModel()
+{
+    return tasks;
+}
+
+AgentModel *CentralKernel::agentModel()
+{
+    return agents;
+}
+
+LoggingModel *CentralKernel::loggingModel()
+{
+    return logger;
+}
+
+ReadyTaskModel *CentralKernel::readyTaskModel()
+{
+    return readyTasks;
+}
+
+WaitingTaskModel *CentralKernel::waitingTaskModel()
+{
+    return waitingTasks;
+}
+
+
+
 void CentralKernel::lock()
 {
     setLocked(true);
@@ -62,4 +88,34 @@ void CentralKernel::setLocked(bool value)
 {
     locked = value;
     emit lockChanged(value);
+}
+
+void CentralKernel::setTaskModel(TaskModel *model)
+{
+    tasks = model;
+    emit taskModelChanged();
+}
+
+void CentralKernel::setAgentModel(AgentModel *model)
+{
+    agents = model;
+    emit agentModelChanged();
+}
+
+void CentralKernel::setLoggingModel(LoggingModel *model)
+{
+    logger = model;
+    emit loggingModelChanged();
+}
+
+void CentralKernel::setReadyTaskModel(ReadyTaskModel *model)
+{
+    readyTasks = model;
+    emit readyTaskModelChanged();
+}
+
+void CentralKernel::setWaitingTaskModel(WaitingTaskModel *model)
+{
+    waitingTasks = model;
+    emit waitingTaskModelChanged();
 }
--- src/core/kernel.h
+++ src/core/kernel.h
@@ -1,58 +1,62 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#include <QObject>
 #include "core/system.h"
-#include "core/taskqueue.hpp"
-#include "core/restqueue.hpp"
 #include "models/taskmodel.h"
 #include "models/agentmodel.h"
 #include "models/loggingmodel.h"
+#include "models/readytaskmodel.h"
+#include "models/waitingtaskmodel.h"
 
 class CentralKernel : public QObject
 {
     Q_OBJECT
 public:
     explicit CentralKernel(const Config::Info &info, QObject *parent = nullptr);
-
-    void addTask(const TaskInfo &info, const QModelIndex &parent);
-    void removeTask(const QModelIndex &index);
-
-    void addAgent(const AgentInfo &info, const QModelIndex &parent);
-    void removeAgent(const QModelIndex &index);
-
-    void shutdown();
-    void execute();
-
     bool isLocked() const;
-
-    TaskModel* showcastTasks();
-    AgentModel* showcastAgents();
-    LoggingModel* showcastLogs();
-    TaskQueue* showcastReadyTasks();
-    RestQueue* showcastWaitingTasks();
+    TaskModel* taskModel();
+    AgentModel* agentModel();
+    LoggingModel* loggingModel();
+    ReadyTaskModel* readyTaskModel();
+    WaitingTaskModel* waitingTaskModel();
 
 public slots:
     void lock();
     void unlock();
     void setLocked(bool value);
+    void setTaskModel(TaskModel* model);
+    void setAgentModel(AgentModel* model);
+    void setLoggingModel(LoggingModel* model);
+    void setReadyTaskModel(ReadyTaskModel* model);
+    void setWaitingTaskModel(WaitingTaskModel *model);
+
+    void addAgent(const AgentInfo &info, const QModelIndex &parent);
+    void addTask(const TaskInfo &info, const QModelIndex &parent);
+    void removeAgent(const QModelIndex &index);
+    void removeTask(const QModelIndex &index);
+    void shutdown();
+    void execute();
 
 signals:
+    void taskModelChanged();
+    void agentModelChanged();
+    void loggingModelChanged();
+    void readyTaskModelChanged();
+    void waitingTaskModelChanged();
+
     void scheduledForShutdown();
     void lockChanged(bool value);
-
 private:
     bool locked;
     qint64 inputLimit;
     Config::Info settings;
     qint64 elapsedQuantum;
     bool scheduleForShutdown;
-
-    TaskModel tasks;
-    AgentModel agents;
-    LoggingModel logger;
-    TaskQueue readyQueue;
-    RestQueue waitingQueue;
+    TaskModel* tasks = nullptr;
+    AgentModel* agents = nullptr;
+    LoggingModel* logger = nullptr;
+    ReadyTaskModel* readyTasks = nullptr;
+    WaitingTaskModel* waitingTasks = nullptr;
 };
 
 #endif // KERNEL_H
