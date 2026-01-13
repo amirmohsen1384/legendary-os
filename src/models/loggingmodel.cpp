fcb45419499add4b54ae65bbee41c83c5de8e090
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-13 10:16:24 +0330

    Completed the logging model

--- src/models/loggingmodel.cpp
+++ src/models/loggingmodel.cpp
@@ -1,35 +1,306 @@
 #include "loggingmodel.h"
+#include <QJsonDocument>
+#include <QJsonObject>
 
-LoggingModel::LoggingModel(QObject *parent)
-    : QAbstractTableModel(parent)
+LoggingModel::LoggingModel(QObject *parent) : QAbstractTableModel(parent)
 {}
 
 QVariant LoggingModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    // FIXME: Implement me!
+    switch(orientation)
+    {
+    case Qt::Vertical:
+    {
+        return section + 1;
+    }
+    case Qt::Horizontal:
+    {
+        switch(role)
+        {
+        case Qt::DisplayRole:
+        {
+            auto group = static_cast<Header>(section);
+            switch (group)
+            {
+            case Header::ID:
+            {
+                return "ID";
+            }
+            case Header::Name:
+            {
+                return "Name";
+            }
+            case Header::Agent:
+            {
+                return "Agent";
+            }
+            case Header::Timestamp:
+            {
+                return "Timestamp";
+            }
+            case Header::Current:
+            {
+                return "Current State";
+            }
+            case Header::Previous:
+            {
+                return "Previous State";
+            }
+            }
+        }
+        case Qt::TextAlignmentRole:
+        {
+            return Qt::AlignCenter;
+        }
+        default:
+        {
+            return {};
+        }
+        }
+    }
+    default:
+    {
+        return {};
+    }
+    }
+}
+
+QByteArray LoggingModel::report() const
+{
+    QJsonDocument document;
+    QJsonObject object;
+    for (const LogInfo &info : container)
+    {
+        const auto key = info.name;
+        QJsonObject value;
+        value.insert("timestamp", info.timestamp);
+        value.insert("id", info.identifier);
+        if (info.agent.isValid())
+        {
+            QJsonObject agent;
+            agent.insert("name", info.agent.getName());
+            agent.insert("description", info.agent.getDescription());
+            value.insert("agent", agent);
+        }
+        value.insert("current", LogInfo::text(info.current));
+        value.insert("previous", LogInfo::text(info.previous));
+        if (!info.description.isEmpty())
+        {
+            value.insert("note", info.description);
+        }
+        object.insert(key, value);
+    }
+    document.setObject(object);
+    return document.toJson();
+}
+
+bool LoggingModel::log(const LogInfo &info)
+{
+    beginInsertRows(QModelIndex(), container.size(), container.size());
+    container.append(info);
+    endInsertRows();
+    return true;
+}
+
+void LoggingModel::clear()
+{
+    beginResetModel();
+    container.clear();
+    endResetModel();
 }
 
 int LoggingModel::rowCount(const QModelIndex &parent) const
 {
     if (parent.isValid())
+    {
         return 0;
-
-    // FIXME: Implement me!
+    }
+    return container.size();
 }
 
 int LoggingModel::columnCount(const QModelIndex &parent) const
 {
     if (parent.isValid())
+    {
         return 0;
+    }
+    auto count = 0;
 
-    // FIXME: Implement me!
+    // Timestamp
+    ++count;
+
+    // ID
+    ++count;
+
+    // Name
+    ++count;
+
+    // Agent
+    ++count;
+
+    // Previous State
+    ++count;
+
+    // Current State
+    ++count;
+
+    return count;
 }
 
 QVariant LoggingModel::data(const QModelIndex &index, int role) const
 {
     if (!index.isValid())
-        return QVariant();
+    {
+        return {};
+    }
+    const auto row = index.row();
+    const auto &data = container[row];
+    switch (role)
+    {
+    case Qt::DisplayRole:
+    {
+        auto group = static_cast<Header>(section);
+        switch (group)
+        {
+        case Header::ID:
+        {
+            return data.identifier;
+        }
+        case Header::Name:
+        {
+            return data.name;
+        }
+        case Header::Agent:
+        {
+            return data.agent.isValid() ? data.agent.getName() : "No Agent";
+        }
+        case Header::Timestamp:
+        {
+            return data.timestamp;
+        }
+        case Header::Current:
+        {
+            return LogInfo::text(data.previous);
+        }
+        case Header::Previous:
+        {
+            return LogInfo::text(data.current);
+        }
+        default:
+        {
+            return {};
+        }
+        }
+        break;
+    }
+    case Qt::TextAlignmentRole:
+    {
+        return Qt::AlignCenter;
+    }
+    case Qt::ForegroundRole:
+    {
+        auto group = static_cast<Header>(section);
+        switch (group)
+        {
+        case Header::Current:
+        {
+            return LogInfo::color(data.current);
+        }
+        case Header::Previous:
+        {
+            return LogInfo::color(data.previous);
+        }
+        default:
+        {
+            return {};
+        }
+        }
+    }
+    case Qt::ToolTipRole:
+    {
+        return data.description;
+    }
+    case Qt::UserRole:
+    {
+        return QVariant::fromValue(data);
+    }
+    default:
+    {
+        return {};
+    }
+    }
+}
 
-    // FIXME: Implement me!
-    return QVariant();
+QString LogInfo::text(const State &info)
+{
+    switch (info)
+    {
+    case State::Error:
+    {
+        return "Error happened";
+    }
+    case State::Executed:
+    {
+        return "Executed";
+    }
+    case State::Ready:
+    {
+        return "Ready";
+    }
+    case State::Running:
+    {
+        return "Running";
+    }
+    case State::Terminated:
+    {
+        return "Finished";
+    }
+    case State::Unknown:
+    {
+        return "Not Known";
+    }
+    case State::WaitingForFile:
+    {
+        return "Waiting for file";
+    }
+    case State::WaitingForLimit:
+    {
+        return "Waiting for limit";
+    }
+    default:
+    {
+        return QString();
+    }
+    }
+}
+
+QColor LogInfo::color(const State &info)
+{
+    switch(info)
+    {
+    case LogInfo::Error:
+    {
+        return QColor(Qt::darkRed);
+    }
+    case LogInfo::Executed:
+    {
+        return QColor(Qt::darkBlue);
+    }
+    case LogInfo::Terminated:
+    {
+        return QColor(Qt::darkMagenta);
+    }
+    case LogInfo::Ready:
+    {
+        return QColor(Qt::darkCyan);
+    }
+    case LogInfo::Running:
+    {
+        return QColor(Qt::darkGreen);
+    }
+    default:
+    {
+        return {};
+    }
+    }
 }
--- src/models/loggingmodel.h
+++ src/models/loggingmodel.h
@@ -2,26 +2,57 @@
 #define LOGGINGMODEL_H
 
 #include <QAbstractTableModel>
+#include "agent.h"
+
+struct LogInfo
+{
+    enum State
+    {
+        Unknown,
+        Ready,
+        Error,
+        Running,
+        Executed,
+        Terminated,
+        WaitingForFile,
+        WaitingForLimit
+    };
+    QString name;
+    AgentInfo agent;
+    qint64 timestamp;
+    qint64 identifier;
+    QString description;
+    State current = State::Unknown;
+    State previous = State::Unknown;
+    static QString text(const State &info);
+    static QColor color(const State &info);
+};
+
+Q_DECLARE_METATYPE(LogInfo)
 
 class LoggingModel : public QAbstractTableModel
 {
     Q_OBJECT
+    enum class Header {Timestamp, Name, Agent, ID, Current, Previous};
 
 public:
     explicit LoggingModel(QObject *parent = nullptr);
 
-    // Header:
-    QVariant headerData(int section,
-                        Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const override;
+public:
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    virtual int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
-    // Basic functionality:
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+public:
+    QByteArray report() const;
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+public slots:
+    bool log(const LogInfo &info);
+    void clear();
 
 private:
+    QList<LogInfo> container;
 };
 
 #endif // LOGGINGMODEL_H
