a2967e10f48daeb5637464ae6d1bf7255300183b
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-16 20:53:56 +0330

    Implemented Test 4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,49 @@
+#include <QTreeView>
+#include <QTableView>
 #include <QApplication>
-#include "src/panels/mainpanel.h"
+#include "src/dialogs/taskedit.h"
+#include "src/models/readymodel.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    Config::initialize();
-    auto config = Config::load();
+    TaskModel model;
+    ReadyModel readyQueue;
 
-    MainPanel panel;
-    panel.show();
+    while (true)
+    {
+        TaskEdit editor(&model);
+        if (editor.exec() == QDialog::Accepted)
+        {
+            model.insertTask(editor.getTaskInfo(), editor.getParent());
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    QTreeView overall;
+    overall.setModel(&model);
+    overall.show();
+
+    QTableView readyView;
+    readyView.setSelectionBehavior(QTableView::SelectRows);
+    readyView.setModel(&readyQueue);
+    readyView.show();
+
+    QObject::connect(&overall, &QTreeView::activated, [&](const QModelIndex &index) {
+        if(!readyQueue.insertTask(index)) {
+            qDebug() << "Failed to add the task.";
+        }
+    });
+
+    QObject::connect(&readyView, &QTableView::activated, [&](const QModelIndex &index) {
+        if (!readyQueue.removeRows(index.row(), 1)) {
+            qDebug() << "Failed to remove the tasks.";
+        }
+    });
 
     return app.exec();
 }
