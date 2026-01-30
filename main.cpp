eda9e177a640311a4af1edb177cd536173055fa9
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2026-01-30 11:43:09 +0330

    Defined some macros to perform different tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include <QTreeView>
-#include <QTableView>
 #include <QApplication>
 #include "src/panels/mainpanel.h"
+#include "src/tests/stresstest.h"
+#include "src/tests/simpletest.h"
+#include "src/tests/dependencytest.h"
+
+
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +13,25 @@ int main(int argc, char *argv[])
 
     auto settings = Settings::load();
     MainPanel panel(settings);
+
+#ifdef STRESS_TEST
+    StressTest test(&panel);
+    test.run();
+#endif
+
+#ifdef SIMPE_TEST
+    SimpleTest test(&panel);
+    test.run();
+#endif
+
+#ifdef DEPENDENCYTEST
+    DependencyTest test(&panel);
+    test.run();
+#endif
+
+#ifdef NORMAL_MODE
     panel.show();
+#endif
 
     return app.exec();
 }
